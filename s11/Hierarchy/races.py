233c1c05040986c748df7e4b64f3b78e08d37933
roy-marquez <romasi@gmail.com>
2020-07-21 20:57:12 -0600

    erase 'pass' keywords

--- s11/Hierarchy/races.py
+++ s11/Hierarchy/races.py
@@ -4,12 +4,12 @@ class JackRussellTerrier(Dog):
     race = 'JackRussellTerrier'
     def speak(self, sound='Argg'):
         return f'{sound} es lo que dice {self.name}'
-    pass
+    
 
 class Dachshund(Dog):
     race = 'Dachshund'
-    pass
+    
 
 class Bulldog(Dog):
     race = 'Bulldog'
-    pass
+    
